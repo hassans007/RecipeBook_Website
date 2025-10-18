e09a30c0558bd14ae0544da30effeb6d95c83e34
Hassan Shahid <u2354758@unimail.hud.ac.uk>
2025-10-18 23:29:27 +0100

    Fix: included style.css import in app.css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
     </title>
     <link rel="icon" href="{{ asset('images/icon/main.ico') }}" type="image/x-icon"/>
     {{-- <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css"/> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'])
 </head>
 <body>
     <nav class="navbar">
