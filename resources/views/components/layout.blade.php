38d1c6645fdb37493138531d5bc71ab315de5420
Hassan Shahid <u2354758@unimail.hud.ac.uk>
2025-10-19 00:14:14 +0100

    Updated the layout and app.js file to check if the style file works or not.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,8 +9,8 @@
         @endisset
     </title>
     <link rel="icon" href="{{ asset('images/icon/main.ico') }}" type="image/x-icon"/>
-    {{-- <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css"/> --}}
-    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css"/>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <nav class="navbar">
